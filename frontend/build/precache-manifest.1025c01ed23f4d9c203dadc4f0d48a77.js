a09da43e5e2361b79f61c593dd27117c342ffb5c
nikhilvenkatkumsetty <43575864+nikhilvenkatkumsetty@users.noreply.github.com>
2020-06-19 19:24:21 +0530

    Add files via upload

---
+++ frontend/build/precache-manifest.1025c01ed23f4d9c203dadc4f0d48a77.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d00047a22f8acdc75a061701ad446fc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c2dcc3a19bb260d3746a",
+    "url": "/static/css/main.a29bb851.chunk.css"
+  },
+  {
+    "revision": "641723a4ccac5fac7e17",
+    "url": "/static/js/2.18cb07c3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.18cb07c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2dcc3a19bb260d3746a",
+    "url": "/static/js/main.1a6295d8.chunk.js"
+  },
+  {
+    "revision": "ec6a910b5fe7fc297b76",
+    "url": "/static/js/runtime-main.61d74c0d.js"
+  }
+]);
---
+++ frontend/src/core/fixedPrices.js
@@ -0,0 +1,32 @@
+﻿export const prices = [
+    {
+        _id: 0,
+        name: "Any",
+        array: []
+    },
+    {
+        _id: 1,
+        name: "₹0 to ₹499",
+        array: [0, 499]
+    },
+    {
+        _id: 2,
+        name: "₹500 to ₹999",
+        array: [501, 999]
+    },
+    {
+        _id: 3,
+        name: "₹1000 to ₹4999",
+        array: [1000, 4999]
+    },
+    {
+        _id: 4,
+        name: "₹5000 to ₹9999",
+        array: [5000, 9999]
+    },
+    {
+        _id: 5,
+        name: "More than ₹10000",
+        array: [10000, 99999]
+    }
+];
